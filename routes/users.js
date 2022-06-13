99a1d4acf3ee19e6d2afdf6e79d6db9ff289cdb2
{自分の名前} <misaki.ishihara0004@gmail.com>
2022-06-13 12:28:59 +0900

    added a followAPI

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ router.put("/:id", async(req, res) => {
 
 //ユーザー情報の削除
 router.delete("/:id", async(req, res) => {
-    if(req.body.useId === req.params.id || req.body.isAdmin) {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
         try {
             const user = await User.findByIdAndDelete(req.params.id);
             res.status(200).json("ユーザー情報が削除されました")
@@ -35,10 +35,44 @@ router.delete("/:id", async(req, res) => {
 router.get("/:id", async(req, res) => {
         try {
             const user = await User.findById(req.params.id);
-            res.status(200).json(user)
+            //secret informations 分割代入でotherだけ取得する
+            const { password, updatedAt, ...other } = user._doc;
+            res.status(200).json(other)
         } catch (err) {
             return res.status(500).json(err)
         }
+})
+
+//ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            //フォローしようとしている人のid
+            const user = await User.findById(req.params.id);
+            //ログイン中の自分のid
+            const currentUser = await User.findById(req.body.userId);
+            //フォロワーに自分がいなかったらフォローできる仕組み
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({
+                    $push: {
+                        followers: req.body.userId,
+                    },
+                })
+                await currentUser.updateOne({
+                    $push: {
+                        followings: req.params.id,
+                    }
+                })
+                return res.status(200).json("フォローできました")
+            } else {
+                return res.status(403).json("フォローしています")
+            }
+        } catch (err) {
+            return res.status(500).json(err)
+        }
+    }  else {
+        return res.status(500).json("error")
+    }
 })
 
 
