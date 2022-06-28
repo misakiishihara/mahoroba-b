d25fc676e4d356d14adbf338585060ecb5e98178
{自分の名前} <misaki.ishihara0004@gmail.com>
2022-06-28 11:37:38 +0900

    github error

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
+const uploadRoute = require("./routes/upload");
 const PORT = 3001;
 const mongoose = require("mongoose");
+const path = require("path")
 
 
 //DB接続
@@ -18,10 +20,12 @@ mongoose.connect(
 });
 
 //middleware
+app.use("/images/", express.static(path.join(__dirname, "public/images")))
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
+app.use("/api/upload", uploadRoute);
 
 
 
